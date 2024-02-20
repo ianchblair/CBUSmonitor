d5013468e0c8a9de8cb269d59d96928d631668dc
ianchblair <68773229+ianchblair@users.noreply.github.com>
2024-02-20 14:52:13 +0000

    Add files via upload
    Minor revisions. Addition of colours. Extended time count.

--- CBUSmonitor/CBUSmonitor.ino
+++ CBUSmonitor/CBUSmonitor.ino
@@ -12,12 +12,12 @@
 // Arduino Data Pins
 //(level shift to 3.3 V required for outputs... )
 //
+// D2  to DC
+// D3  to CS
 // D4  to CAN TX (CANTX0)
 // D5  to CAN RX (CANRX0)
 // 
 // D8  to Display RST
-// S9  to DC
-// D10 to CS
 // D11 to MOSI
 // D12 to MISO
 // D13 to SCK
@@ -35,8 +35,8 @@
 #define SCREEN_WIDTH 360 // OLED display width, in pixels
 #define SCREEN_HEIGHT 240 // OLED display height, in pixels
 
-#define TFT_CS 3 // ou D10
-#define TFT_DC 2 // Or D9?
+#define TFT_CS 3  // ou D10
+#define TFT_DC 2  // Or D9?
 #define TFT_RST 8 // D8
 Adafruit_ILI9341 tftdisplay = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST); 
 static uint32_t CanId = 1; // Temporary value
@@ -56,11 +56,11 @@ void display_setup() {
 
 // Display buffer
 // Can message has 13 bytes. We allow 48 bytes when converted to hex (+count or timestamp?)
-#define TFTDISPLAY_WIDTH 48   // Width in characters 
+#define TFTDISPLAY_WIDTH 49   // Width in characters +1 for string terminator 
 // We allow for 32 lines on display, each of any colout
 #define TFTDISPLAY_LENGTH 32  // Number of lines (must be a power of two)
 #define TFTDISPLAY_MASK (TFTDISPLAY_LENGTH-1)
-typedef struct display_line {uint16_t text_colour; char text_data[48];};
+typedef struct display_line {uint16_t text_colour; char text_data[TFTDISPLAY_WIDTH];};
 display_line displayBuffer[TFTDISPLAY_LENGTH];
 display_line * displayPtr = &displayBuffer[0];
 
@@ -93,8 +93,40 @@ void display_all()
 // Allocates colour according to opcode category and value
 uint16_t text_colour(uint8_t opcode)
 {
+  uint16_t colour;
 // Categories are config, layout control, locomotive control and bus control
 // layout On and off we allocate specific colours to (green for on, red for off)
+  switch (opcode)
+  {
+  // Some Config messages
+  case OPC_RQNN:
+  case OPC_RQNP:
+  case OPC_PARAMS:
+  case OPC_NAME:
+  case OPC_SNN:
+  case OPC_NNACK:
+    colour = ILI9341_MAGENTA;
+    break;
+  // Layout control ON
+  case OPC_ACON:
+  case OPC_ASON:
+    colour = ILI9341_GREEN;
+    break;
+  // layout  control OFF
+  case OPC_ACOF:
+  case OPC_ASOF:
+    colour = ILI9341_RED;
+    break;
+  // Traction messages
+  case OPC_RDCC3:
+  case OPC_RDCC4:
+  case OPC_RDCC5:
+    colour - ILI9341_BLUE;
+  default:
+    colour = ILI9341_BLACK;
+    break;
+  }
+  return(colour);
 }
 
 void can_setup() {
@@ -114,26 +146,35 @@ void setup() {
 static uint32_t msg_cnt = 0;
 
 // This next section runs continuosly
-void loop() {
-  /* Assemble a CAN message with the format of
-   * 0xCA 0xFE 0x00 0x00 [4 byte message counter]
-   */
-  // Use an RQNN for test purposes
-  uint8_t const msg_data[] = {0x50,0,0};
-  CanMsg const msg(CanStandardId(CanId), sizeof(msg_data), msg_data);
-
-  /* Transmit the CAN message, capture and display an
-   * error core in case of failure.
-   */
-  if (int const rc = CAN.write(msg); rc < 0)
+void loop() 
+{
+  // Read serial port for commanded test functions
+  if (Serial.available())
   {
-    Serial.print  ("CAN.write(...) failed with error code ");
-    Serial.println(rc);
-    // for (;;) { };
-  }
+    char c = Serial.read();
+    // Only one implemented so far 
+    // It a 't' is typed send a test message
+    if (c == 't')
+    {
+      /* Assemble a CAN message with the format of
+       * 0xCA 0xFE 0x00 0x00 [4 byte message counter]
+       */
+      // Use an RQNN for test purposes
+      uint8_t const msg_data[] = {0x50,0,0};
+      CanMsg const msg(CanStandardId(CanId), sizeof(msg_data), msg_data);
 
-  /* Increase the message counter. */
-  msg_cnt++;
+      /* Transmit the CAN message, capture and display an
+       * error core in case of failure.
+      */
+      if (int const rc = CAN.write(msg); rc < 0)
+      {
+        Serial.print  ("CAN.write(...) failed with error code ");
+        Serial.println(rc);
+        // for (;;) { };
+      }
+    }
+  }
+ 
 
 // Flag to mark when display update required
   boolean update_display = false;
@@ -149,7 +190,7 @@ void loop() {
     char disp_data[TFTDISPLAY_WIDTH];
     char* disp_ptr = disp_data;
     // Note that Can ID has 32 bits
-    disp_ptr += sprintf(disp_ptr, "%5d %03hx", msg_cnt, msg.id);    
+    disp_ptr += sprintf(disp_ptr, "%10d %03hx", msg_cnt, msg.id);    
     for(size_t i = 0; i < msgLength; i++)  
     {
       disp_ptr += sprintf(disp_ptr, " %02hx", msg.data[i]);
@@ -165,12 +206,14 @@ void loop() {
   // you can change the delay for a longer or shorter clock cycle ( 5000 is approx 5 seconds )
   
   // char test_data[TFTDISPLAY_WIDTH];
-  // sprintf(test_data, " %5d hhh oo dd dd dd dd dd dd dd dd", msg_cnt);
+  // sprintf(test_data, " %10d hhh oo dd dd dd dd dd dd dd", msg_cnt);
   // display_add(ILI9341_BLACK, test_data);
   // display_add(msg_cnt, test_data);
   if (update_display == true) display_all();
+   /* Increase the loop counter. */
+  msg_cnt++;
   // delay to be relooked at!
   // remove or set to 1?
-  delay (1000);
+  delay (10);
 }
  
